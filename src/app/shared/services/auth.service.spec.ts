803acf5f06e334f4d40b2a2d67ee792bf26d417a
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-10-14 21:31:54 +0300

    add: services specs were added

---
+++ src/app/shared/services/auth.service.spec.ts
@@ -0,0 +1,10 @@
+import {AuthService} from "./auth.service";
+
+describe('AuthService', () => {
+
+  let service: AuthService;
+
+  beforeEach(() => {
+    service = new AuthService(null, null, null);
+  })
+})
---
+++ src/app/shared/services/posts.service.spec.ts
@@ -0,0 +1,11 @@
+import {PostService} from "./post.service";
+
+describe('PostsService', () => {
+
+  let service: PostService;
+
+  beforeEach(() => {
+    service = new PostService(null);
+  })
+
+})

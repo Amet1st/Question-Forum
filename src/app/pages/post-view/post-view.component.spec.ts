233319995504ce21a8a3ea91f3e0b43de7cc5d80
Nikolay Naidenov <mikalay.naydzenau@gmail.com>
2022-10-16 17:09:29 +0300

    add: spec file for post-view was added

---
+++ src/app/pages/post-view/post-view.component.spec.ts
@@ -0,0 +1,11 @@
+import {PostViewComponent} from "./post-view.component";
+
+describe('PostViewComponent', () => {
+  let component: PostViewComponent;
+
+  beforeEach(() => {
+    component = new PostViewComponent(null, null, null, null, null, null);
+  })
+
+
+})
